78f9e443eaa6c7cc483c7c638adde46127a31fce
Vincent <va543567@gmail.com>
2024-11-26 16:02:29 +0000

    feat: convert CityDao object to City object

--- src/main/java/com/northcoders/demospringbootapp/controller/CityController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/CityController.java
@@ -2,6 +2,7 @@ package com.northcoders.demospringbootapp.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.demospringbootapp.DAO.CityDao;
 import com.northcoders.demospringbootapp.DAO.GeoResultDao;
 import model.City;
 import org.springframework.http.MediaType;
@@ -22,6 +23,7 @@ public class CityController {
     @GetMapping("/info")
     @ResponseBody
     public List<City> getCityInfo(@RequestParam(name = "name") String name, @RequestParam(name = "count") Integer count) {
+        List<City> cityList = new ArrayList<>();
 
         // receive the city name & count from the user
         System.out.println(name);
@@ -44,18 +46,23 @@ public class CityController {
 
         System.out.println(cityInfoStr);
 
-        // convert the WebClient objects to our City model
         try {
+            // convert the WebClient string to CityDao
             GeoResultDao resultDao = mapper.readValue(cityInfoStr, GeoResultDao.class);
             System.out.println(resultDao);
+
+            // convert the CityDao to our City model
+            for (CityDao dao : resultDao.results()) {
+                City city = new City(dao);
+                cityList.add(city);
+            }
 
         } catch (JsonProcessingException e) {
             System.err.println(e);
         }
 
         // return back to the client
-
-        return new ArrayList<>();
+        return cityList;
     }
 
 }
--- src/main/java/model/City.java
+++ src/main/java/model/City.java
@@ -1,13 +1,63 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.northcoders.demospringbootapp.DAO.CityDao;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record City(
-    int id,
-    String name,
-    double latitude,
-    double longitude,
-    String country
-) {
+public class City {
+    private long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String country;
+
+    public City(CityDao dao) {
+        if (dao != null) {
+            this.id = dao.id();
+            this.name = dao.name();
+            this.latitude = dao.latitude();
+            this.longitude = dao.longitude();
+            this.country = dao.country();
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
